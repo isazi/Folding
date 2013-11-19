4e935e9e29b060809cc926b6d0f7318c125e8a53
Alessio Sclocco <alessio@sclocco.eu>
2013-11-19 12:48:38 +0100

    Forgot a space.

--- tuning/Folding.cpp
+++ tuning/Folding.cpp
@@ -237,7 +237,7 @@ int main(int argc, char * argv[]) {
 			}
 			Vcur = sqrt(Vcur / nrIterations);
 
-			cout << observation.getNrDMs() << " " << observation.getNrPeriods() << " " << observation.getBasePeriod() << " " << observation.getFirstPeriod() << " " << observation.getPeriodStep() << observation.getNrBins() << " " << observation.getNrSamplesPerSecond() << " " << (*parameters)[0] << " " << (*parameters)[1] << " " << (*parameters)[2] << " " << (*parameters)[3] << " " << (*parameters)[4] << " " << (*parameters)[5] << " " << setprecision(3) << Acur << " " << Vcur << " " << setprecision(6) << clFold.getTimer().getAverageTime() << " " << clFold.getTimer().getStdDev() << endl;
+			cout << observation.getNrDMs() << " " << observation.getNrPeriods() << " " << observation.getBasePeriod() << " " << observation.getFirstPeriod() << " " << observation.getPeriodStep() << " " << observation.getNrBins() << " " << observation.getNrSamplesPerSecond() << " " << (*parameters)[0] << " " << (*parameters)[1] << " " << (*parameters)[2] << " " << (*parameters)[3] << " " << (*parameters)[4] << " " << (*parameters)[5] << " " << setprecision(3) << Acur << " " << Vcur << " " << setprecision(6) << clFold.getTimer().getAverageTime() << " " << clFold.getTimer().getStdDev() << endl;
 		} catch ( OpenCLError err ) {
 			cerr << err.what() << endl;
 			continue;
