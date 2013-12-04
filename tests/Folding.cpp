ccae331fa13ab00c9bfac5cd37711e29ff22a4ba
Alessio Sclocco <alessio@sclocco.eu>
2013-12-04 12:25:11 +0100

    Code not modified, just some reordering of the includes.

--- tests/Folding.cpp
+++ tests/Folding.cpp
@@ -1,41 +1,39 @@
-/*
- * Copyright (C) 2012
- * Alessio Sclocco <a.sclocco@vu.nl>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+//
+// Copyright (C) 2012
+// Alessio Sclocco <a.sclocco@vu.nl>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #include <iostream>
-#include <string>
-#include <vector>
-#include <exception>
-#include <fstream>
-#include <iomanip>
-#include <limits>
-#include <cmath>
-#include <ctime>
 using std::cout;
 using std::cerr;
 using std::endl;
+#include <string>
 using std::string;
+#include <vector>
 using std::vector;
+#include <exception>
 using std::exception;
-using std::ofstream;
+#include <iomanip>
 using std::fixed;
 using std::setprecision;
+#include <limits>
 using std::numeric_limits;
+#include <cmath>
+#include <ctime>
+
 
 #include <ArgumentList.hpp>
 using isa::utils::ArgumentList;
