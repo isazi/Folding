e88c4014263da301c4265ac1d8b049412b3f9d79
Alessio Sclocco <alessio@sclocco.eu>
2013-12-04 12:24:52 +0100

    Adding a test for the AVX folding.

---
+++ tests/FoldingAVX.cpp
@@ -0,0 +1,139 @@
+//
+// Copyright (C) 2013
+// Alessio Sclocco <a.sclocco@vu.nl>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <exception>
+using std::exception;
+#include <iomanip>
+using std::fixed;
+using std::setprecision;
+#include <limits>
+using std::numeric_limits;
+#include <cmath>
+#include <ctime>
+
+#include <ArgumentList.hpp>
+using isa::utils::ArgumentList;
+#include <Observation.hpp>
+using AstroData::Observation;
+#include <InitializeOpenCL.hpp>
+using isa::OpenCL::initializeOpenCL;
+#include <CLData.hpp>
+using isa::OpenCL::CLData;
+#include <utils.hpp>
+using isa::utils::same;
+#include <Folding.hpp>
+using PulsarSearch::Folding;
+#include <FoldingCPU.hpp>
+using PulsarSearch::folding;
+#include <FoldingAVX.hpp>
+using PulsarSearch::folding;
+#include <Bins.hpp>
+using PulsarSearch::getNrSamplesPerBin;
+
+typedef float dataType;
+const string typeName("float");
+const unsigned int padding = 8;
+
+// LOFAR
+//const unsigned int nrSamplesPerSecond = 200000;
+// Apertif
+const unsigned int nrSamplesPerSecond = 20000;
+// DMs
+const unsigned int nrDMs = 256;
+// Periods
+const unsigned int nrPeriods = 128;
+const unsigned int nrBins = 256;
+const unsigned int periodStep = 64;
+
+
+int main(int argc, char *argv[]) {
+	long long unsigned int wrongValues = 0;
+	Observation< dataType > observation("FoldingTest", typeName);
+	CLData< dataType > * dedispersedData = new CLData< dataType >("DedispersedData", true);
+	CLData< dataType > * foldedDataCPU = new CLData<dataType >("FoldedDataCPU", true);
+	CLData< dataType > * foldedDataAVX = new CLData<dataType >("FoldedDataAVX", true);
+	CLData< unsigned int > * readCounterData = new CLData< unsigned int >("ReadCounterData", true);
+	CLData< unsigned int > * writeCounterData = new CLData< unsigned int >("WriteCounterData", true);
+	CLData< unsigned int > * nrSamplesPerBin = new CLData< unsigned int >("NrSamplesPerBin", true);
+
+	// Setup of the observation
+	observation.setPadding(padding);
+	observation.setNrSamplesPerSecond(nrSamplesPerSecond);
+	observation.setNrDMs(nrDMs);
+	observation.setNrPeriods(nrPeriods);
+	observation.setFirstPeriod(nrBins);
+	observation.setPeriodStep(periodStep);
+	observation.setNrBins(nrBins);
+	
+
+	// Allocate memory
+	dedispersedData->allocateHostData(observation.getNrSamplesPerSecond() * observation.getNrPaddedDMs());
+	foldedDataCPU->allocateHostData(observation.getNrPaddedDMs() * observation.getNrBins() * observation.getNrPeriods());
+	foldedDataCPU->blankHostData();
+	foldedDataAVX->allocateHostData(observation.getNrPaddedDMs() * observation.getNrBins() * observation.getNrPeriods());
+	foldedDataAVX->blankHostData();
+	readCounterData->allocateHostData(observation.getNrPeriods() * 2 * observation.getNrPaddedBins());
+	readCounterData->blankHostData();
+	writeCounterData->allocateHostData(observation.getNrPeriods() * 2 * observation.getNrPaddedBins());
+	writeCounterData->blankHostData();
+	vector< unsigned int > * nrSamplesPerBinData = getNrSamplesPerBin(observation);
+	nrSamplesPerBin->allocateHostData(*nrSamplesPerBinData);
+
+	srand(time(NULL));
+	for ( unsigned int sample = 0; sample < observation.getNrSamplesPerSecond(); sample++ ) {
+		for ( unsigned int DM = 0; DM < observation.getNrDMs(); DM++ ) {
+			dedispersedData->setHostDataItem((sample * observation.getNrPaddedDMs()) + DM, rand() % 100);
+		}
+	}
+
+	// Test & Check
+	folding(0, observation, dedispersedData->getHostData(), foldedDataCPU->getHostData(), writeCounterData->getHostData());
+	folding(0, observation, dedispersedData->getHostData(), foldedDataAVX->getHostData(), readCounterData->getHostData(), writeCounterData->getHostData());
+	for ( unsigned int bin = 0; bin < observation.getNrBins(); bin++ ) {
+		long long unsigned int wrongValuesBin = 0;
+
+		for ( unsigned int period = 0; period < observation.getNrPeriods(); period++ ) {
+			for ( unsigned int DM = 0; DM < observation.getNrDMs(); DM++ ) {
+				const unsigned int dataItem = (bin * observation.getNrPeriods() * observation.getNrPaddedDMs()) + (period * observation.getNrPaddedDMs()) + DM;
+				if ( !same(foldedDataCPU->getHostDataItem(dataItem), foldedDataAVX->getHostDataItem(dataItem)) ) {
+					wrongValues++;
+					wrongValuesBin++;
+				}
+			}
+		}
+
+		if ( wrongValuesBin > 0 ) {
+			cout << "Wrong samples bin " << bin << ": " << wrongValuesBin << " (" << (wrongValuesBin * 100) / (static_cast< long long unsigned int >(observation.getNrDMs()) * observation.getNrPeriods()) << "%)." << endl;
+		}
+	}	
+
+	cout << endl;
+	cout << "Wrong samples: " << wrongValues << " (" << (wrongValues * 100) / (static_cast< long long unsigned int >(observation.getNrDMs()) * observation.getNrPeriods() * observation.getNrBins()) << "%)." << endl;
+	cout << endl;
+
+	return 0;
+}
+
