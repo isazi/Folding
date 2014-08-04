fc60403abffcb6a29f62bd97e8a0b8423bb40a42
Alessio Sclocco <alessio@sclocco.eu>
2014-08-04 17:03:55 +0200

    Almost done with the OpenCL tuner.

--- tuning/Folding.cpp
+++ tuning/Folding.cpp
@@ -19,218 +19,214 @@
 #include <fstream>
 #include <iomanip>
 #include <limits>
-#include <cmath>
-using std::cout;
-using std::cerr;
-using std::endl;
-using std::string;
-using std::vector;
-using std::exception;
-using std::ofstream;
-using std::fixed;
-using std::setprecision;
-using std::numeric_limits;
+#include <algorithm>
 
 #include <ArgumentList.hpp>
-using isa::utils::ArgumentList;
 #include <Observation.hpp>
-using AstroData::Observation;
 #include <InitializeOpenCL.hpp>
-using isa::OpenCL::initializeOpenCL;
-#include <CLData.hpp>
-using isa::OpenCL::CLData;
-#include <utils.hpp>
-using isa::utils::toStringValue;
-#include <Folding.hpp>
-using PulsarSearch::Folding;
-#include <Timer.hpp>
-using isa::utils::Timer;
+#include <Kernel.hpp>
 #include <Bins.hpp>
-using PulsarSearch::getNrSamplesPerBin;
+#include <Folding.hpp>
+#include <utils.hpp>
+#include <Exceptions.hpp>
+#include <Timer.hpp>
+#include <Stats.hpp>
 
 typedef float dataType;
-const string typeName("float");
+string typeName("float");
 
 
 int main(int argc, char * argv[]) {
-	unsigned int lowerNrThreads = 0;
-	unsigned int maxThreadsPerBlock = 0;
-	unsigned int maxItemsPerThread = 16;
-	unsigned int maxColumns = 0;
-	unsigned int maxRows = 0;
 	unsigned int nrIterations = 0;
 	unsigned int clPlatformID = 0;
 	unsigned int clDeviceID = 0;
-	Observation< dataType > observation("FoldingTuning", typeName);
-	CLData< dataType > * dedispersedData = new CLData< dataType >("DedispersedData", true);
-	CLData< dataType > * foldedData = new CLData<dataType >("FoldedData", true);
-	CLData< unsigned int > * readCounterData = new CLData< unsigned int >("ReadCounterData", true);
-	CLData< unsigned int > * writeCounterData = new CLData< unsigned int >("WriteCounterData", true);
-	CLData< unsigned int > * nrSamplesPerBin = new CLData< unsigned int >("SamplesPerBin", true);
-
+	unsigned int minThreads = 0;
+	unsigned int maxThreadsPerBlock = 0;
+	unsigned int maxItemsPerThread = 0;
+	unsigned int maxColumns = 0;
+	unsigned int maxRows = 0;
+  AstroData::Observation< dataType > observation("FoldingTuning", typeName);
 
 	try {
-		ArgumentList args(argc, argv);
+    isa::utils::ArgumentList args(argc, argv);
 
+		nrIterations = args.getSwitchArgument< unsigned int >("-iterations");
 		clPlatformID = args.getSwitchArgument< unsigned int >("-opencl_platform");
 		clDeviceID = args.getSwitchArgument< unsigned int >("-opencl_device");
 		observation.setPadding(args.getSwitchArgument< unsigned int >("-padding"));
-		nrIterations = args.getSwitchArgument< unsigned int >("-iterations");
-		observation.setNrDMs(args.getSwitchArgument< unsigned int >("-dms"));
-		lowerNrThreads = args.getSwitchArgument< unsigned int >("-lnt");
-		maxThreadsPerBlock = args.getSwitchArgument< unsigned int >("-mnt");
-		maxItemsPerThread = args.getSwitchArgument< unsigned int >("-mit");
+		minThreads = args.getSwitchArgument< unsigned int >("-min_threads");
+		maxThreadsPerBlock = args.getSwitchArgument< unsigned int >("-max_threads");
+		maxItemsPerThread = args.getSwitchArgument< unsigned int >("-max_items");
 		maxColumns = args.getSwitchArgument< unsigned int >("-max_columns");
 		maxRows = args.getSwitchArgument< unsigned int >("-max_rows");
-		observation.setNrPeriods(args.getSwitchArgument< unsigned int >("-periods"));
-		observation.setFirstPeriod(args.getSwitchArgument< unsigned int >("-first_period"));
-		observation.setPeriodStep(args.getSwitchArgument< unsigned int >("-period_step"));
-		observation.setNrBins(args.getSwitchArgument< unsigned int >("-bins"));
-		observation.setNrSamplesPerSecond(args.getSwitchArgument< unsigned int >("-samples"));
-	} catch ( exception & err ) {
-		cerr << err.what() << endl;
+    observation.setNrSamplesPerSecond(args.getSwitchArgument< unsigned int >("-samples"));
+		observation.setNrDMs(args.getSwitchArgument< unsigned int >("-dms"));
+    observation.setNrPeriods(args.getSwitchArgument< unsigned int >("-periods"));
+    observation.setNrBins(args.getSwitchArgument< unsigned int >("-bins"));
+    observation.setFirstPeriod(args.getSwitchArgument< unsigned int >("-first_period"));
+    observation.setPeriodStep(args.getSwitchArgument< unsigned int >("-period_step"));
+	} catch ( isa::Exceptions::EmptyCommandLine &err ) {
+		std::cerr << argv[0] << " -iterations ... -opencl_platform ... -opencl_device ... -padding ... -min_threads ... -max_threads ... -max_items ... -max_columns ... -max_rows ... -samples ... -dms ... -periods ... -bins ... -first_period ... -period_step ..." << std::endl;
+		return 1;
+	} catch ( std::exception &err ) {
+		std::cerr << err.what() << std::endl;
 		return 1;
 	}
 
+	// Initialize OpenCL
 	cl::Context * clContext = new cl::Context();
-	vector< cl::Platform > * clPlatforms = new vector< cl::Platform >();
-	vector< cl::Device > * clDevices = new vector< cl::Device >();
-	vector< vector< cl::CommandQueue > > * clQueues = new vector< vector < cl::CommandQueue > >();
+	std::vector< cl::Platform > * clPlatforms = new std::vector< cl::Platform >();
+	std::vector< cl::Device > * clDevices = new std::vector< cl::Device >();
+	std::vector< std::vector< cl::CommandQueue > > * clQueues = new std::vector< std::vector < cl::CommandQueue > >();
 
-	initializeOpenCL(clPlatformID, 1, clPlatforms, clContext, clDevices, clQueues);
-
-	cout << fixed << endl;
-	cout << "# nrDMs nrPeriods firstPeriod periodStep nrBins nrSamplesPerSecond nrDMsPerBlock nrPeriodsPerBlock nrBinsPerBlock nrDMsPerThread nrPeriodsPerThread nrBinsPerThread GFLOP/s err time err" << endl << endl;
+  isa::OpenCL::initializeOpenCL(clPlatformID, 1, clPlatforms, clContext, clDevices, clQueues);
+  std::vector< unsigned int > * samplesPerBin = PulsarSearch::getSamplesPerBin(observation);
 
 	// Allocate memory
-	dedispersedData->allocateHostData(observation.getNrSamplesPerSecond() * observation.getNrPaddedDMs());
-	dedispersedData->blankHostData();
-	foldedData->allocateHostData(observation.getNrPaddedDMs() * observation.getNrBins() * observation.getNrPeriods());
-	foldedData->blankHostData();
-	readCounterData->allocateHostData(observation.getNrPeriods() * observation.getNrPaddedBins());
-	readCounterData->blankHostData();
-	writeCounterData->allocateHostData(observation.getNrPeriods() * observation.getNrPaddedBins());
-	writeCounterData->blankHostData();
-	vector< unsigned int > * nrSamplesPerBinData = getNrSamplesPerBin(observation);
-	nrSamplesPerBin->allocateHostData(*nrSamplesPerBinData);
+  cl::Buffer samplesPerBin_d;
+  std::vector< dataType > dedispersedData = std::vector< dataType >(observation.getNrSamplesPerSecond() * observation.getNrPaddedDMs());
+  cl::Buffer dedispersedData_d;
+  std::vector< dataType > foldedData = std::vector< dataType >(observation.getNrBins() * observation.getNrPeriods() * observation.getNrPaddedDMs());
+  cl::Buffer foldedData_d;
+  std::vector< unsigned int > readCounters = std::vector< unsigned int >(observation.getNrBins() * observation.getNrPeriods() * observation.getNrPaddedDMs());
+  cl::Buffer readCounters_d;
+  std::vector< unsigned int > writeCounters = std::vector< unsigned int >(observation.getNrBins() * observation.getNrPeriods() * observation.getNrPaddedDMs());
+  cl::Buffer writeCounters_d;
+  try {
+    samplesPerBin_d = cl::Buffer(*clContext, CL_MEM_READ_ONLY, samplesPerBin->size() * sizeof(unsigned int), NULL, NULL);
+    dedispersedData_d = cl::Buffer(*clContext, CL_MEM_READ_WRITE, dedispersedData.size() * sizeof(dataType), NULL, NULL);
+    foldedData_d = cl::Buffer(*clContext, CL_MEM_READ_WRITE, foldedData.size() * sizeof(dataType), NULL, NULL);
+    readCounters_d = cl::Buffer(*clContext, CL_MEM_READ_WRITE, readCounters.size() * sizeof(unsigned int), NULL, NULL);
+    writeCounters_d = cl::Buffer(*clContext, CL_MEM_READ_WRITE, writeCounters.size() * sizeof(unsigned int), NULL, NULL);
+  } catch ( cl::Error &err ) {
+    std::cerr << "OpenCL error allocating memory: " << isa::utils::toString< cl_int >(err.err()) << "." << std::endl;
+    return 1;
+  }
 
-	dedispersedData->setCLContext(clContext);
-	dedispersedData->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	foldedData->setCLContext(clContext);
-	foldedData->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	readCounterData->setCLContext(clContext);
-	readCounterData->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	writeCounterData->setCLContext(clContext);
-	writeCounterData->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	nrSamplesPerBin->setCLContext(clContext);
-	nrSamplesPerBin->setCLQueue(&((clQueues->at(clDeviceID)).at(0)));
-	nrSamplesPerBin->setDeviceReadOnly();
-
-	try {
-		dedispersedData->allocateDeviceData();
-		dedispersedData->copyHostToDevice();
-		foldedData->allocateDeviceData();
-		foldedData->copyHostToDevice();
-		readCounterData->allocateDeviceData();
-		readCounterData->copyHostToDevice();
-		writeCounterData->allocateDeviceData();
-		writeCounterData->copyHostToDevice();
-		nrSamplesPerBin->allocateDeviceData();
-		nrSamplesPerBin->copyHostToDevice();
-	} catch ( OpenCLError err ) {
-		cerr << err.what() << endl;
+	srand(time(NULL));
+  for ( unsigned int sample = 0; sample < observation.getNrSamplesPerSecond(); sample++ ) {
+    for ( unsigned int DM = 0; DM < observation.getNrDMs(); DM++ ) {
+      dedispersedData[(sample * observation.getNrPaddedDMs()) + DM] = static_cast< dataType >(rand() % 10);
+		}
 	}
+  std::fill(foldedData.begin(), foldedData.end(), static_cast< dataType >(0));
+  std::fill(readCounters.begin(), readCounters.end(), 0);
+  std::fill(writeCounters.begin(), writeCounters.end(), 0);
+
+  // Copy data structures to device
+  try {
+    clQueues->at(clDeviceID)[0].enqueueWriteBuffer(samplesPerBin_d, CL_FALSE, 0, samplesPerBin->size() * sizeof(unsigned int), reinterpret_cast< void * >(samplesPerBin->data()));
+    clQueues->at(clDeviceID)[0].enqueueWriteBuffer(dedispersedData_d, CL_FALSE, 0, dedispersedData.size() * sizeof(dataType), reinterpret_cast< void * >(dedispersedData.data()));
+    clQueues->at(clDeviceID)[0].enqueueWriteBuffer(foldedData_d, CL_FALSE, 0, foldedData.size() * sizeof(dataType), reinterpret_cast< void * >(foldedData.data()));
+    clQueues->at(clDeviceID)[0].enqueueWriteBuffer(readCounters_d, CL_FALSE, 0, readCounters->size() * sizeof(unsigned int), reinterpret_cast< void * >(readCounters->data()));
+    clQueues->at(clDeviceID)[0].enqueueWriteBuffer(writeCounters_d, CL_FALSE, 0, writeCounters->size() * sizeof(unsigned int), reinterpret_cast< void * >(writeCounters->data()));
+  } catch ( cl::Error &err ) {
+    std::cerr << "OpenCL error H2D transfer: " << isa::utils::toString< cl_int >(err.err()) << "." << std::endl;
+    return 1;
+  }
 
 	// Find the parameters
-	vector< vector< unsigned int > > configurations;
-	for ( unsigned int DMsPerBlock = lowerNrThreads; DMsPerBlock <= maxColumns; DMsPerBlock += lowerNrThreads ) {
-		if ( observation.getNrPaddedDMs() % DMsPerBlock != 0 ) {
-			continue;
+	std::vector< unsigned int > DMsPerBlock;
+	for ( unsigned int DMs = minThreads; DMs <= maxColumns; DMs += minThreads ) {
+		if ( (observation.getNrPaddedDMs() % DMs) == 0 ) {
+			DMsPerBlock.push_back(DMs);
 		}
-
-		for ( unsigned int periodsPerBlock = 1; periodsPerBlock <= maxRows; periodsPerBlock++ ) {
-			if ( observation.getNrPeriods() % periodsPerBlock != 0 ) {
-				continue;
-			} else if ( DMsPerBlock * periodsPerBlock > maxThreadsPerBlock ) {
-				break;
-			}
-
-			for ( unsigned int binsPerBlock = 1; binsPerBlock <= maxRows; binsPerBlock++ ) {
-				if ( observation.getNrBins() % binsPerBlock != 0 ) {
-					continue;
-				} else if ( DMsPerBlock * periodsPerBlock * binsPerBlock > maxThreadsPerBlock ) {
-					break;
-				}
-
-				for ( unsigned int DMsPerThread = 1; DMsPerThread <= maxItemsPerThread; DMsPerThread++ ) {
-					if ( observation.getNrPaddedDMs() % (DMsPerBlock * DMsPerThread) != 0 ) {
-						continue;
-					}
-
-					for ( unsigned int periodsPerThread = 1; periodsPerThread <= maxItemsPerThread; periodsPerThread++ ) {
-						if ( observation.getNrPeriods() % (periodsPerBlock * periodsPerThread) != 0 ) {
-							continue;
-						} else if ( (DMsPerThread + (2 * periodsPerThread)) > maxItemsPerThread ) {
-							break;
-						}
-
-						for ( unsigned int binsPerThread = 1; binsPerThread <= maxItemsPerThread; binsPerThread++ ) {
-							if ( observation.getNrBins() % (binsPerBlock * binsPerThread) != 0 ) {
-								continue;
-							} else if ( (DMsPerThread + (2 * periodsPerThread) + binsPerThread) + (3 * periodsPerThread * binsPerThread) + (2 * DMsPerThread * periodsPerThread * binsPerThread) > maxItemsPerThread ) {
-								break;
-							}
-
-							vector< unsigned int > parameters;
-
-							parameters.push_back(DMsPerBlock);
-							parameters.push_back(periodsPerBlock);
-							parameters.push_back(binsPerBlock);
-							parameters.push_back(DMsPerThread);
-							parameters.push_back(periodsPerThread);
-							parameters.push_back(binsPerThread);
-
-							configurations.push_back(parameters);
-						}
-					}
-				}
-			}
+	}
+	std::vector< unsigned int > periodsPerBlock;
+	for ( unsigned int periods = 1; periods <= maxRows; periods++ ) {
+		if ( (observation.getNrperiods() % periods) == 0 ) {
+			periodsPerBlock.push_back(periods);
+		}
+	}
+	std::vector< unsigned int > binsPerBlock;
+	for ( unsigned int bins = 1; bins <= maxRows; bins++ ) {
+		if ( (observation.getNrbins() % bins) == 0 ) {
+			binsPerBlock.push_back(bins);
 		}
 	}
 
-	for ( vector< vector< unsigned int > >::const_iterator parameters = configurations.begin(); parameters != configurations.end(); parameters++ ) {
-		try {
-			// Generate kernel
-			Folding< dataType > clFold("clFold", typeName);
-			clFold.bindOpenCL(clContext, &(clDevices->at(clDeviceID)), &((clQueues->at(clDeviceID)).at(0)));
-			clFold.setObservation(&observation);
-			clFold.setNrSamplesPerBin(nrSamplesPerBin);
-			clFold.setNrDMsPerBlock((*parameters)[0]);
-			clFold.setNrPeriodsPerBlock((*parameters)[1]);
-			clFold.setNrBinsPerBlock((*parameters)[2]);
-			clFold.setNrDMsPerThread((*parameters)[3]);
-			clFold.setNrPeriodsPerThread((*parameters)[4]);
-			clFold.setNrBinsPerThread((*parameters)[5]);
-			clFold.generateCode();
+	std::cout << std::fixed << std::endl;
+	std::cout << "# nrDMs nrSamples nrPeriods nrBins firstPeriod periodStep DMsPerBlock periodsPerBlock binsPerBlock DMsPerThread periodsPerThread binsPerThread GFLOP/s err time err" << std::endl << std::endl;
 
-			foldedData->copyHostToDevice();
-			clFold(0, dedispersedData, foldedData, readCounterData, writeCounterData);
-			(clFold.getTimer()).reset();
-			clFold.resetStats();
+  for ( std::vector< unsigned int >::iterator DMs = DMsPerBlock.begin(); DMs != DMsPerBlock.end(); ++DMs ) {
+    for ( std::vector< unsigned int >::iterator periods = periodsPerBlock.begin(); periods != periodsPerBlock.end(); ++periods ) {
+      for ( std::vector< unsigned int >::iterator bins = binsPerBlock.begin(); bins != binsPerBlock.end(); ++bins ) {
+        if ( (*DMs * *periods * *bins) > maxThreadsPerBlock ) {
+          break;
+        }
+        for ( unsigned int DMsPerThread = 1; DMsPerThread <= maxItemsPerThread; DMsPerThread++ ) {
+          if ( observation.getNrPaddedDMs() % (*DMs * DMsPerThread) != 0 ) {
+            continue;
+          }
+          for ( unsigned int periodsPerThread = 1; periodsPerThread <= maxItemsPerThread; periodsPerThread++ ) {
+            if ( observation.getNrPeriods() % (*periods * periodsPerThread) != 0 ) {
+              continue;
+            }
+            for ( unsigned int binsPerThread = 1; binsPerThread <= maxItemsPerThread; binsPerThread++ ) {
+              if ( observation.getNrBins() % (*bins * binsPerThread) != 0 ) {
+                continue;
+              }
+              // TODO: complete the expression.
+              if ( () > maxItemsPerThread ) {
+                break;
+              }
+              // Generate kernel
+              cl::Kernel * kernel;
+              std::string * code = PulsarSearch::getFoldingOpenCL(*DMs, *periods, *bins, DMsPerThread, periodsPerThread, binsPerThread, typeName, observation);
 
-			for ( unsigned int iteration = 0; iteration < nrIterations; iteration++ ) {
-				foldedData->copyHostToDevice();
-				clFold(0, dedispersedData, foldedData, readCounterData, writeCounterData);
-			}
+              try {
+                kernel = isa::OpenCL::compile("folding", *code, "-cl-mad-enable -Werror", *clContext, clDevices->at(clDeviceID));
+              } catch ( isa::Exceptions::OpenCLError &err ) {
+                std::cerr << err.what() << std::endl;
+                continue;
+              }
 
-			cout << observation.getNrDMs() << " " << observation.getNrPeriods() << " " << observation.getFirstPeriod() << " " << observation.getPeriodStep() << " " << observation.getNrBins() << " " << observation.getNrSamplesPerSecond() << " " << (*parameters)[0] << " " << (*parameters)[1] << " " << (*parameters)[2] << " " << (*parameters)[3] << " " << (*parameters)[4] << " " << (*parameters)[5] << " " << setprecision(3) << clFold.getGFLOPs() << " " << clFold.getGFLOPsErr() << " " << setprecision(6) << clFold.getTimer().getAverageTime() << " " << clFold.getTimer().getStdDev() << endl;
-		} catch ( OpenCLError err ) {
-			cerr << err.what() << endl;
-			continue;
-		}
-	}
+              cl::NDRange global(observation.getNrPaddedDMs() / DMsPerThread, observation.getNrPeriods() / periodsPerThread, observation.getNrBins() / binsPerThread);
+              cl::NDRange local(*DMs, *periods, *bins);
 
-	cout << endl;
+              kernel->setArg(0, 0);
+              kernel->setArg(1, dedispersedData_d);
+              kernel->setArg(2, foldedData_d);
+              kernel->setArg(3, readCounters_d);
+              kernel->setArg(4, writeCounters_d);
+              kernel->setArg(5, samplesPerBin_d);
+
+              // Warm-up run
+              try {
+                clQueues->at(clDeviceID)[0].enqueueNDRangeKernel(*kernel, cl::NullRange, global, local);
+              } catch ( cl::Error &err ) {
+                std::cerr << "OpenCL error kernel execution: " << isa::utils::toString< cl_int >(err.err()) << "." << std::endl;
+                continue;
+              }
+              // Tuning runs
+              double flops = isa::utils::giga(static_cast< long long unsigned int >(observation.getNrDMs()) * observation.getNrPeriods() * observation.getNrSamplesPerSecond());
+              isa::utils::Timer timer("Kernel Timer");
+              isa::utils::Stats< double > stats;
+              cl::Event event;
+
+              try {
+                for ( unsigned int iteration = 0; iteration < nrIterations; iteration++ ) {
+                  timer.start();
+                  clQueues->at(clDeviceID)[0].enqueueNDRangeKernel(*kernel, cl::NullRange, global, local, NULL, &event);
+                  event.wait();
+                  timer.stop();
+                  stats.addElement(flops / timer.getLastRunTime());
+                }
+              } catch ( cl::Error &err ) {
+                std::cerr << "OpenCL error kernel execution: " << isa::utils::toString< cl_int >(err.err()) << "." << std::endl;
+                continue;
+              }
+
+              std::cout << observation.getNrDMs() << " " << observation.getNrSamplesPerSecond() << " " << observation.getNrPeriods() << " " << observation.getNrBins() << " " << observation.getFirstPeriod() << " " << observation.getPeriodStep() << " " << *DMs << " " << *periods << " " << *bins << " " << DMsPerThread << " " << periodsPerThread << " " << binsPerThread << " " << std::setprecision(3) << stats.getAverage() << " " << stats.getStdDev() << " " << std::setprecision(6) << timer.getAverageTime() << " " << timer.getStdDev() << std::endl;
+            }
+          }
+        }
+      }
+    }
+  }
+
+	std::cout << std::endl;
 
 	return 0;
 }
+
