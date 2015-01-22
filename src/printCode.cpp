45bd0f4d70872b8aa43e96a1253a7621d4cf2b67
Alessio Sclocco <alessio@sclocco.eu>
2015-01-22 14:31:55 +0100

    Refactored printCode.

--- src/printCode.cpp
+++ src/printCode.cpp
@@ -28,27 +28,21 @@
 
 
 int main(int argc, char *argv[]) {
-  unsigned int vector = 0;
-  unsigned int nrDMsPerBlock = 0;
-  unsigned int nrPeriodsPerBlock = 0;
-  unsigned int nrBinsPerBlock = 0;
-  unsigned int nrDMsPerThread = 0;
-  unsigned int nrPeriodsPerThread = 0;
-  unsigned int nrBinsPerThread = 0;
   std::string typeName;
+  PulsarSearch::FoldingConf conf;
 	AstroData::Observation observation;
 
 	try {
     isa::utils::ArgumentList args(argc, argv);
     typeName = args.getSwitchArgument< std::string >("-type");
     observation.setPadding(args.getSwitchArgument< unsigned int >("-padding"));
-    vector = args.getSwitchArgument< unsigned int >("-vector");
-    nrDMsPerBlock = args.getSwitchArgument< unsigned int >("-db");
-    nrPeriodsPerBlock = args.getSwitchArgument< unsigned int >("-pb");
-    nrBinsPerBlock = args.getSwitchArgument< unsigned int >("-bb");
-    nrDMsPerThread = args.getSwitchArgument< unsigned int >("-dt");
-    nrPeriodsPerThread = args.getSwitchArgument< unsigned int >("-pt");
-    nrBinsPerThread = args.getSwitchArgument< unsigned int >("-bt");
+    conf.setVector(args.getSwitchArgument< unsigned int >("-vector"));
+    conf.setNrDMsPerBlock(args.getSwitchArgument< unsigned int >("-db"));
+    conf.setNrPeriodsPerBlock(args.getSwitchArgument< unsigned int >("-pb"));
+    conf.setNrBinsPerBlock(args.getSwitchArgument< unsigned int >("-bb"));
+    conf.setNrDMsPerThread(args.getSwitchArgument< unsigned int >("-dt"));
+    conf.setNrPeriodsPerThread(args.getSwitchArgument< unsigned int >("-pt"));
+    conf.setNrBinsPerThread(args.getSwitchArgument< unsigned int >("-bt"));
     observation.setNrSamplesPerSecond(args.getSwitchArgument< unsigned int >("-samples"));
     observation.setDMRange(args.getSwitchArgument< unsigned int >("-dms"), 0.0, 0.0);
     observation.setPeriodRange(args.getSwitchArgument< unsigned int >("-periods"), args.getSwitchArgument< unsigned int >("-first_period"), args.getSwitchArgument< unsigned int >("-period_step"));
@@ -62,7 +56,7 @@ int main(int argc, char *argv[]) {
 	}
 
   // Generate kernel
-  std::string * code = PulsarSearch::getFoldingOpenCL(nrDMsPerBlock, nrPeriodsPerBlock, nrBinsPerBlock, nrDMsPerThread, nrPeriodsPerThread, nrBinsPerThread, vector, typeName, observation);
+  std::string * code = PulsarSearch::getFoldingOpenCL(conf, typeName, observation);
   std::cout << *code << std::endl;
 
 	return 0;
